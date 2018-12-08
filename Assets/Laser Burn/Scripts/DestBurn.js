0ecb76578f268140d49960ee06a40c3f70dadd51
ashlinaronin <ashlinaronin@gmail.com>
2018-12-08 11:51:35 -0800

    Remove cached files from git that are in gitignore

--- Assets/Laser Burn/Scripts/DestBurn.js
+++
@@ -1,9 +0,0 @@
-﻿#pragma strict
-
-var UseLifeTime = false;
-var lifetime = 2;
-
-function Start () {
-if(UseLifeTime)
-Destroy (gameObject, lifetime);
-}
--- Assets/LaserPointerVR/scripts/RoomTeleportActionable.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-public class RoomTeleportActionable : Selectable, IActionable {
-  public void Action(ActionContext ctx) {
-    print("Room teleport action handler!");
-    playerLogic.RoomTeleport(gameObject.name);
-  }
-}
