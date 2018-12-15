27dd6f60dbbc418afaa6b140127dd5e36225ef73
unknown <xhurch@gmail.com>
2018-12-15 15:39:14 -0800

    Detect when cross is upside down and print message to console

---
+++ Assets/DetectRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.localEulerAngles.z > 180.0 && transform.localEulerAngles.z < 220.0) {
+			print("cross is upside down");
+		}
+	}
+}
