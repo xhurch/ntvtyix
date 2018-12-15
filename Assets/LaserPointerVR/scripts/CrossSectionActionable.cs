6815957e3226e5c4289cea6e3e4040d20af1b0f8
XHURCH <info@xhurch.net>
2018-12-15 12:47:34 -0800

    Interaction Engine version

--- Assets/LaserPointerVR/scripts/CrossSectionActionable.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-public class CrossSectionActionable : Selectable, IActionable {
-  public void Action(ActionContext ctx) {
-    print("Cross section action handler!");
-    playerLogic.RoomTeleport(gameObject.name);
-  }
-}
